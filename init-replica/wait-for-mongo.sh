525ccfad48bbd414b8c14751faddfabc26bf3ce4
AbedAwaisy <abed.awaisy.a@gmail.com>
2024-06-11 01:11:47 +0300

    prepare for dev pipeline

---
+++ init-replica/wait-for-mongo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until mongo --host "$host" --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)'; do
+  >&2 echo "Mongo is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Mongo is up - executing command"
+exec $cmd
